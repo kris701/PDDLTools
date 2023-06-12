ebfcba4fdcc6cef40077e0e47e65814c73da6f2e
Kristian Skov <kris701kj@gmail.com>
2023-06-12 12:16:49 +0200

    Added initial type checking for the domain

--- PDDLParser/Analysers/PostParsingAnalyser.cs
+++ PDDLParser/Analysers/PostParsingAnalyser.cs
@@ -14,8 +14,7 @@ namespace PDDLParser.Analysers
     {
         public static void AnalyseDomain(DomainDecl domain, IErrorListener listener)
         {
-            if (domain.Types != null)
-                CheckDeclaredVsUsedTypes(domain, listener);
+            CheckDeclaredVsUsedTypes(domain, listener);
         }
 
         private static void CheckDeclaredVsUsedTypes(DomainDecl domain, IErrorListener listener)
@@ -24,23 +23,84 @@ namespace PDDLParser.Analysers
             // Default in PDDL
             declaredTypes.Add("object");
             declaredTypes.Add("");
-            foreach (var typeDecl in domain.Types.Types)
-                foreach (var type in typeDecl.SubTypes)
-                    declaredTypes.Add(type);
+            if (domain.Types != null)
+                foreach (var typeDecl in domain.Types.Types)
+                    foreach (var type in typeDecl.SubTypes)
+                        declaredTypes.Add(type);
 
             // Check predicates
-            foreach(var predicate in domain.Predicates.Predicates)
+            if (domain.Predicates != null)
             {
-                foreach(var arg in predicate.Arguments)
+                foreach (var predicate in domain.Predicates.Predicates)
                 {
-                    if (!declaredTypes.Contains(arg.Type))
+                    foreach (var arg in predicate.Arguments)
+                    {
+                        if (!declaredTypes.Contains(arg.Type))
+                        {
+                            listener.AddError(new ParseError(
+                                $"Use of undeclared type '{arg.Type}'",
+                                ParserErrorLevel.High,
+                                ParseErrorType.Error,
+                                arg.Line,
+                                arg.Character));
+                        }
+                    }
+                }
+            }
+
+            // Check constants
+            if (domain.Constants != null)
+            {
+                foreach(var constant in domain.Constants.Constants)
+                {
+                    if (!declaredTypes.Contains(constant.Type))
                     {
                         listener.AddError(new ParseError(
-                            $"Use of undeclared type '{arg.Type}'",
+                            $"Use of undeclared type '{constant.Type}'",
                             ParserErrorLevel.High,
                             ParseErrorType.Error,
-                            arg.Line,
-                            arg.Character));
+                            constant.Line,
+                            constant.Character));
+                    }
+                }
+            }
+
+            // Check actions
+            if (domain.Actions != null)
+            {
+                foreach (var action in domain.Actions)
+                {
+                    foreach(var param in action.Parameters)
+                    {
+                        if (!declaredTypes.Contains(param.Type))
+                        {
+                            listener.AddError(new ParseError(
+                                $"Use of undeclared type '{param.Type}'",
+                                ParserErrorLevel.High,
+                                ParseErrorType.Error,
+                                param.Line,
+                                param.Character));
+                        }
+                    }
+                }
+            }
+
+            // Check axioms
+            if (domain.Axioms != null)
+            {
+                foreach (var axiom in domain.Axioms)
+                {
+                    foreach (var variable in axiom.Vars)
+                    {
+                        if (!declaredTypes.Contains(variable.Type))
+                        {
+                            listener.AddError(new ParseError(
+                                $"Use of undeclared type '{variable.Type}'",
+                                ParserErrorLevel.High,
+                                ParseErrorType.Error,
+                                variable.Line,
+                                variable.Character));
+                        }
                     }
                 }
             }
