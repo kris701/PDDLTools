b9cb8460316e588b81e2c6dbd9b903c1342ae8b5
Kristian Skov <kris701kj@gmail.com>
2023-06-17 10:43:11 +0200

    Added a completer service for the simple PDDL syntax

---
+++ PDDLTools/Completers/PDDLCompletionHandlerProvider.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLTools.Completers
+{
+    // https://learn.microsoft.com/en-us/visualstudio/extensibility/walkthrough-displaying-statement-completion?view=vs-2022&tabs=csharp
+    [Export(typeof(IVsTextViewCreationListener))]
+    [Name("PDDL Completion Handler")]
+    [ContentType(Constants.PDDLLanguageName)]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    internal class PDDLCompletionHandlerProvider : IVsTextViewCreationListener
+    {
+        [Import]
+        internal IVsEditorAdaptersFactoryService AdapterService = null;
+        [Import]
+        internal ICompletionBroker CompletionBroker { get; set; }
+        [Import]
+        internal SVsServiceProvider ServiceProvider { get; set; }
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            ITextView textView = AdapterService.GetWpfTextView(textViewAdapter);
+            if (textView == null)
+                return;
+
+            Func<PDDLCompletionCommandHandler> createCommandHandler = delegate () { return new PDDLCompletionCommandHandler(textViewAdapter, textView, this); };
+            textView.Properties.GetOrCreateSingletonProperty(createCommandHandler);
+        }
+    }
+}
---
+++ PDDLTools/Completers/PDDLCompletionSourceProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLTools.Completers
+{
+    // https://learn.microsoft.com/en-us/visualstudio/extensibility/walkthrough-displaying-statement-completion?view=vs-2022&tabs=csharp
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType(Constants.PDDLLanguageName)]
+    [Name("PDDL Completion")]
+    internal class PDDLCompletionSourceProvider : ICompletionSourceProvider
+    {
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new PDDLCompletionSource(this, textBuffer);
+        }
+    }
+}
