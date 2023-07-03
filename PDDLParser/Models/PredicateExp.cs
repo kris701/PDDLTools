fce1b2986d4bcd2d75cecd8c5f588894ed204f84
Kristian Skov <kris701kj@gmail.com>
2023-07-03 14:50:04 +0200

    Refactoring

--- PDDLParser/Models/PredicateExp.cs
+++ PDDLParser/Models/PredicateExp.cs
@@ -46,7 +46,7 @@ namespace PDDLParser.Models
 
         public object Clone()
         {
-            var newPredicateExp = new PredicateExp(new ASTNode(Start, Line, ""), Parent, Name, new List<NameExp>());
+            var newPredicateExp = new PredicateExp(new ASTNode(Start, End, Line, ""), Parent, Name, new List<NameExp>());
             foreach (var arg in Arguments)
                 newPredicateExp.Arguments.Add(new NameExp(new ASTNode(arg.Start, arg.End, arg.Line, ""), newPredicateExp, arg.Name));
             return newPredicateExp;
