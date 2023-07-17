19ce52338cd6a877a3c3515014c1dab9a6566d02
Kristian Skov <kris701kj@gmail.com>
2023-07-17 12:26:20 +0200

    WIP

---
+++ PDDLTools.TestAdapter/PDDLToolsTestDiscoverer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLTools.TestAdapter
+{
+    [FileExtension(".dll")]
+    [FileExtension(".exe")]
+    [DefaultExecutorUri(PDDLToolsTestExecutor.ExecutorUri)]
+    public sealed class PDDLToolsTestDiscoverer : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger messageLogger, ITestCaseDiscoverySink discoverySink)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PDDLTools.TestAdapter/PDDLToolsTestExecutor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLTools.TestAdapter
+{
+    [ExtensionUri(ExecutorUri)]
+    internal class PDDLToolsTestExecutor : ITestExecutor
+    {
+        public const string ExecutorUri = "executor://PDDLToolsTestExecutor";
+
+        public void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
