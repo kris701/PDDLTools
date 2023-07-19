32d19ceef71e02028af1f9a4be9c7cc9bb42a80a
Kristian Skov <kris701kj@gmail.com>
2023-07-19 20:40:35 +0200

    Got the entire base discovery process to work!

---
+++ PDDLTools.TestAdapter/Models/PDDLTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLTools.TestAdapter.Models
+{
+    public class PDDLTest
+    {
+        public string Domain { get; set; }
+        public List<string> Problems { get; set; }
+    }
+}
--- PDDLTools.TestAdapter/PDDLToolsTestDiscoverer.cs
+++ PDDLTools.TestAdapter/PDDLToolsTestDiscoverer.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PDDLTools.TestAdapter.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,33 @@ namespace PDDLTools.TestAdapter
         {
             TestLog.Initialize(messageLogger);
             Initialize(discoveryContext);
-            TestLog.SendMessage(TestMessageLevel.Informational, "works!");
+            TestLog.SendInformationalMessage("Discovering PDDL test files...");
+            int counter = 1;
             foreach (var file in sources)
-                TestLog.SendMessage(TestMessageLevel.Informational, $"   File: {file}");
+            {
+                TestLog.SendInformationalMessage($"Generating test case {counter++} out of {sources.Count()}");
+                try
+                {
+                    GenerateTestCasesFromTestFile(file, discoverySink);
+                }
+                catch (Exception ex)
+                {
+                    TestLog.SendErrorMessage($"An error occured while discovering tests: {ex}");
+                }
+            }
+        }
+
+        private void GenerateTestCasesFromTestFile(string file, ITestCaseDiscoverySink discoverySink)
+        {
+            PDDLTest test = JsonConvert.DeserializeObject<PDDLTest>(File.ReadAllText(file));
+            var basePath = new FileInfo(test.Domain).Name;
+            foreach(var problem in test.Problems)
+            {
+                var caseName = Path.Combine(basePath, new FileInfo(problem).Name.Replace(".pddl", ""));
+                var newCase = new TestCase(caseName, new Uri(PDDLToolsTestExecutor.ExecutorUriStr), file.ToLowerInvariant());
+                newCase.CodeFilePath = problem;
+                discoverySink.SendTestCase(newCase);
+            }
         }
     }
 }
